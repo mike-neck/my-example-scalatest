767d31647620d7d656fb08863b98bb681a268cb5
mike_neck <jkrt3333@gmail.com>
2022-05-06 10:05:11 +0900

    Avoid conflict of formatted method

--- src/main/scala/com/example/DefaultRule.scala
+++ src/main/scala/com/example/DefaultRule.scala
@@ -4,7 +4,7 @@ case class DefaultRule(mod: Int, text: String) extends Rule {
   override def apply(value: Value): Value =
     if (value.original % mod == 0) {
       value.current match {
-        case Some(v) => Value("%s%s".formatted(v, text), value.original)
+        case Some(v) => Value(s"$v$text", value.original)
         case None => Value(text, value.original)
       }
     } else value
